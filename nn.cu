ebfe635325369e6af35f51dc661c9fde693d4915
Oli Clive-Griffin <o.clivegriffin@gmail.com>
2024-09-16 01:09:47 +1200

    wip

--- nn.cu
+++ nn.cu
@@ -8,54 +8,103 @@
 
 #define TILE_WIDTH 16
 
-#define CEIL_DIV(numer, denom) (numer + denom - 1) / (denom)
+#define CEIL_DIV(numer, denom) ((numer + denom - 1) / (denom))
 
-cudaError_t err;
-
-
-__global__ void fusedLinear(
-    float *in, // (batch, in,)
-    float *W, // (in, out)
-    float *b, // (out,)
-    float *out, // (out,)
+__global__ void fusedBareLinear(
+    const float *in, // (batch, in)
+    const float *W,  // (in, out)
+    const float *b,  // (out,)
+    float *out,      // (batch, out)
     int nBatches,
-    int inDim,
-    int outDim,
+    int inFeatures,
+    int outFeatures
 ) {
     __shared__ float sharedW[TILE_WIDTH][TILE_WIDTH];
     __shared__ float sharedIn[TILE_WIDTH][TILE_WIDTH];
 
 
-    int by = blockIdx.y; int thY = threadIdx.y;
-    int bx = blockIdx.x; int thX = threadIdx.x;
+    int blY = blockIdx.y; int thY = threadIdx.y;
+    int blX = blockIdx.x; int thX = threadIdx.x;
 
-    int batchIdx = by * TILE_WIDTH + thY;
-    int outFeatureIdx = bx * TILE_WIDTH + thX;
+    int batchIdx = blY * TILE_WIDTH + thY;
+    int outFeatureIdx = blX * TILE_WIDTH + thX;
 
     float sum = 0.0f;
-    for (int tileIdx = 0; tileIdx < CEIL_DIV(K, TILE_WIDTH) + 1; ++tileIdx) {
+    for (int tileIdx = 0; tileIdx < CEIL_DIV(inFeatures, TILE_WIDTH); ++tileIdx) {
         int inFeatureIdxIn = tileIdx * TILE_WIDTH + thX;
-        bool shouldLoadIn = batchIdx < nBatches && inFeatureIdxIn < inDim;
-        sharedIn[thY][thX] = shouldLoadA ? a[batchIdx * inDim + inFeatureIdxIn] : 0.0f;
+        bool shouldLoadIn = batchIdx < nBatches && inFeatureIdxIn < inFeatures;
+        // sharedIn[thY][thX] = shouldLoadIn * in[batchIdx * inFeatures + inFeatureIdxIn];
+        sharedIn[thY][thX] = shouldLoadIn ? in[batchIdx * inFeatures + inFeatureIdxIn] : 0.0f;
 
         int inFeatureIdxW = tileIdx * TILE_WIDTH + thY;
-        bool shouldLoadW = outFeatureIdx < outDim && inFeatureIdxW < inDim;
-        sharedW[thY][thX] = shouldLoadB ? b[inFeatureIdxW * outDim + outFeatureIdx] : 0.0f;
+        bool shouldLoadW = outFeatureIdx < outFeatures && inFeatureIdxW < inFeatures;
+        // sharedW[thY][thX] = shouldLoadW * W[inFeatureIdxW * outFeatures + outFeatureIdx];
+        sharedW[thY][thX] = shouldLoadW ? W[inFeatureIdxW * outFeatures + outFeatureIdx] : 0.0f;
 
         __syncthreads();
-
+        
+        #pragma unroll
         for (int k = 0; k < TILE_WIDTH; ++k)
-            sum += sharedIn[thY][k] * sharedW[k][thX]
+            sum += sharedIn[thY][k] * sharedW[k][thX];
 
         __syncthreads();
     }
 
     sum += b[outFeatureIdx]; // bias
 
-    sum *= (sum > 0); // ReLU
+    int outIdx = batchIdx * outFeatures + outFeatureIdx;
+    if (batchIdx < nBatches && outFeatureIdx < outFeatures)
+        out[outIdx] = sum;
+}
 
-    int outIdx = batchIdx * outDim + outFeatureIdx;
-    if (batchIdx < batches && col < outDim)
+
+
+__global__ void fusedLinearRelu(
+    const float *in, // (batch, in)
+    const float *W,  // (in, out)
+    const float *b,  // (out,)
+    float *out,      // (batch, out)
+    int nBatches,
+    int inFeatures,
+    int outFeatures
+) {
+    __shared__ float sharedW[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float sharedIn[TILE_WIDTH][TILE_WIDTH];
+
+
+    int blY = blockIdx.y; int thY = threadIdx.y;
+    int blX = blockIdx.x; int thX = threadIdx.x;
+
+    int batchIdx = blY * TILE_WIDTH + thY;
+    int outFeatureIdx = blX * TILE_WIDTH + thX;
+
+    float sum = 0.0f;
+    for (int tileIdx = 0; tileIdx < CEIL_DIV(inFeatures, TILE_WIDTH); ++tileIdx) {
+        int inFeatureIdxIn = tileIdx * TILE_WIDTH + thX;
+        bool shouldLoadIn = batchIdx < nBatches && inFeatureIdxIn < inFeatures;
+        // sharedIn[thY][thX] = shouldLoadIn * in[batchIdx * inFeatures + inFeatureIdxIn];
+        sharedIn[thY][thX] = shouldLoadIn ? in[batchIdx * inFeatures + inFeatureIdxIn] : 0.0f;
+
+        int inFeatureIdxW = tileIdx * TILE_WIDTH + thY;
+        bool shouldLoadW = outFeatureIdx < outFeatures && inFeatureIdxW < inFeatures;
+        // sharedW[thY][thX] = shouldLoadW * W[inFeatureIdxW * outFeatures + outFeatureIdx];
+        sharedW[thY][thX] = shouldLoadW ? W[inFeatureIdxW * outFeatures + outFeatureIdx] : 0.0f;
+
+        __syncthreads();
+        
+        #pragma unroll
+        for (int k = 0; k < TILE_WIDTH; ++k)
+            sum += sharedIn[thY][k] * sharedW[k][thX];
+
+        __syncthreads();
+    }
+
+    sum += b[outFeatureIdx]; // bias
+
+    sum = fmaxf(sum, 0.0f); // ReLU
+
+    int outIdx = batchIdx * outFeatures + outFeatureIdx;
+    if (batchIdx < nBatches && outFeatureIdx < outFeatures)
         out[outIdx] = sum;
 }
 
@@ -71,77 +120,97 @@ void initMatrixZeros(float *mat, int n_elem) {
         mat[i] = 0;
 }
 
-void linear(
+void launchLinearRelu(
     int nBatches,
-    int inDim,
-    int outDim,
     float* d_in,
-    float* d_W,
-    float* d_b,
-    float* d_out
+    float* d_out,
+    Layer layer
 ) {
-    dim3 gridDim_(CEIL_DIV(N, TILE_WIDTH), CEIL_DIV(M, TILE_WIDTH))
+    dim3 gridDim_(CEIL_DIV(nBatches, TILE_WIDTH), CEIL_DIV(layer.outFeatures, TILE_WIDTH));
     dim3 blockDim_(TILE_WIDTH, TILE_WIDTH);
 
-    fusedLinear<<<gridDim_, blockDim_>>>(
-        d_in, d_W, d_b, d_out,
-        nBatches, inDim, outDim
+    fusedLinearRelu<<<gridDim_, blockDim_>>>(
+        d_in, layer.d_W, layer.d_b, d_out,
+        nBatches, layer.inFeatures, layer.outFeatures
     );
 }
 
+void launchBareLinear(
+    int nBatches,
+    float* d_in,
+    float* d_out,
+    Layer layer
+) {
+    dim3 gridDim_(CEIL_DIV(nBatches, TILE_WIDTH), CEIL_DIV(layer.outFeatures, TILE_WIDTH));
+    dim3 blockDim_(TILE_WIDTH, TILE_WIDTH);
+
+    fusedBareLinear<<<gridDim_, blockDim_>>>(
+        d_in, layer.d_W, layer.d_b, d_out,
+        nBatches, layer.inFeatures, layer.outFeatures
+    );
+}
+
+
+typedef struct Layer {
+    int inFeatures;
+    int outFeatures;
+    float* d_W;
+    float* d_b;
+} Layer;
+
 typedef struct NN {
     int sizeIn;
-    float* d_W1;
-    float* d_b1;
-    int size1;
-    float* d_W2;
-    float* d_b2;
-    int size2;
-    float* d_W3;
-    float* d_b3;
-    int sizeOut;
+    int numLayers;
+    Layer* layers;
 } NN;
 
 
-void initLayer(int sizeIn, int sizeOut, float** d_W, float** d_b) {
+Layer initLayer(int sizeIn, int sizeOut) {
     size_t sizeW = sizeof(float) * sizeIn * sizeOut;
     size_t sizeB = sizeof(float) * sizeOut;
 
     float* h_W = (float*)malloc(sizeW);
     initMatrixRandom(h_W, sizeIn * sizeOut);
 
-    err = cudaMalloc(d_W, sizeW);
-    if (err != cudaSuccess) {
-        printf("error `cudaMalloc`ing d_W. error: %s\n", cudaGetErrorString(err));
-    }
+    float* d_W;
+    cudaError_t err1 = cudaMalloc(d_W, sizeW);
+    if (err1 != cudaSuccess)
+        printf("error `cudaMalloc`ing d_W. error: %s\n", cudaGetErrorString(err1));
+    cudaError_t err2 = cudaMemcpy(*d_W, h_W, sizeW, cudaMemcpyHostToDevice);
+    if (err2 != cudaSuccess)
+        printf("error `cudaMemcpy`ing to d_W. error: %s\n", cudaGetErrorString(err2));
 
-    err = cudaMemcpy(*d_W, h_W, sizeW, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
-        printf("error `cudaMemcpy`ing to d_W. error: %s\n", cudaGetErrorString(err));
-    }
+    free(h_W);
 
     float* h_b = (float*)malloc(sizeB);
     initMatrixZeros(h_b, sizeOut);
 
-    err = cudaMalloc(d_b, sizeB);
-    if (err != cudaSuccess) {
-        printf("error `cudaMalloc`ing d_b. error: %s\n", cudaGetErrorString(err));
-    }
+    float* d_b;
+    cudaError_t err3 = cudaMalloc(d_b, sizeB);
+    if (err3 != cudaSuccess)
+        printf("error `cudaMalloc`ing d_b. error: %s\n", cudaGetErrorString(err3));
+    cudaError_t err4 = cudaMemcpy(*d_b, h_b, sizeB, cudaMemcpyHostToDevice);
+    if (err4 != cudaSuccess)
+        printf("error `cudaMemcpy`ing to d_b. error: %s\n", cudaGetErrorString(err4));
 
-    err = cudaMemcpy(*d_b, h_b, sizeB, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
-        printf("error `cudaMemcpy`ing to d_b. error: %s\n", cudaGetErrorString(err));
-    }
+    free(h_b);
+
+    Layer layer;
+    layer.inFeatures = sizeIn;
+    layer.outFeatures = sizeOut;
+    layer.d_W = *d_W;
+    layer.d_b = *d_b;
+
+    return layer;
 }
 
 
 void printDeviceArr(float* d_x, int n_elem, char* name) {
     size_t size = sizeof(float) * n_elem;
     float* h_x = (float*)malloc(size);
-    err = cudaMemcpy(h_x, d_x, size, cudaMemcpyDeviceToHost);
-    if (err != cudaSuccess) {
+    cudaError_t err = cudaMemcpy(h_x, d_x, size, cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess)
         printf("error `cudaMemcpy`ing to d_b. error: %s\n", cudaGetErrorString(err));
-    }
     printf("%s:\n", name);
     for (int i = 0; i < n_elem; ++i)
         printf("%f ", h_x[i]);
@@ -152,108 +221,111 @@ void printDeviceArr(float* d_x, int n_elem, char* name) {
 
 NN* createNN(
     int sizeIn,
-    int sizeLayer1,
-    int sizeLayer2,
-    int sizeOut
+    int numLayers,
+    int *layerSizes
 ) {
     NN* nn = (NN*)malloc(sizeof(NN));
 
     nn->sizeIn = sizeIn;
-    nn->size1 = sizeLayer1;
-    nn->size2 = sizeLayer2;
+    nn->numLayers = numLayers;
+    nn->layers = (Layer*)malloc(sizeof(Layer) * numLayers);
 
-    initLayer(sizeIn, sizeLayer1, &nn->d_W1, &nn->d_b1);
-    initLayer(sizeLayer1, sizeLayer2, &nn->d_W2, &nn->d_b2);
-    initLayer(sizeLayer2, sizeOut, &nn->d_W3, &nn->d_b3);
-    printDeviceArr(nn->d_W1, nn->sizeIn * nn->size1, (char*)"d_W1");
-    printDeviceArr(nn->d_W2, nn->size1 * nn->size2, (char*)"d_W2");
+    nn->layers[0] = initLayer(sizeIn, layerSizes[0]);
+    for (int i = 1; i < numLayers; ++i) {
+        nn->layers[i] = initLayer(layerSizes[i-1], layerSizes[i]);
+    }
 
-    nn->sizeOut = sizeOut;
     return nn;
 }
 
 
 void freeNN(NN* nn) {
-    free(nn->d_W1);
-    free(nn->d_b1);
-    free(nn->d_W2);
-    free(nn->d_b2);
-    free(nn->d_W3);
-    free(nn->d_b3);
+    for (int i = 0; i < nn->numLayers; ++i) {
+        cudaFree(nn->layers[i].d_W);
+        cudaFree(nn->layers[i].d_b);
+    }
+
+    free(nn->layers);
     free(nn);
 }
 
 
 void runNN(
     NN* nn,
+    int nBatches,
     float* d_in,
     float* d_out
 ) {
-    float *d_h1, *d_h2;
+    for (int i = 0; i < nn->numLayers - 1; ++i) {
+        Layer layer = nn->layers[i];
+        float *d_out;
 
-    err = cudaMalloc(&d_h1, sizeof(float) * nn->size1);
-    if (err != cudaSuccess) {
-        printf("error `cudaMemcpy`ing to d_b. error: %s\n", cudaGetErrorString(err));
+        cudaError_t err1 = cudaMalloc(&d_out, sizeof(float) * nn->layer.outFeatures);
+        if (err1 != cudaSuccess)
+            printf("error cudaMallocing d_out. error: %s\n", cudaGetErrorString(err1));
+
+        launch(
+            nBatches,
+            layer.inFeatures,
+            layer.outFeatures,
+            d_in,
+            layer.d_W,
+            layer.d_b,
+            d_out
+        );
+
+        d_in = d_out;
+
+        // tricky, need to free the output of the previous layer
     }
 
-    err = cudaMalloc(&d_h2, sizeof(float) * nn->size2);
-    if (err != cudaSuccess) {
-        printf("error `cudaMemcpy`ing to d_b. error: %s\n", cudaGetErrorString(err));
-    }
 
-    linear(nn->sizeIn, nn->size1, d_in, nn->d_W1, nn->d_b1, d_h1);
-    linear(nn->size1, nn->size2, d_h1, nn->d_W2, nn->d_b2, d_h2);
-    linear(nn->size2, nn->sizeOut, d_h2, nn->d_W3, nn->d_b3, d_out);
 
-    cudaFree(d_h1);
     cudaFree(d_h2);
 }
 
 
 int main() {
-    int inDim = 3;
+    int inFeatures = 3;
     int hiddenDim1 = 5;
     int hiddenDim2 = 3;
-    int outDim = 10;
+    int outFeatures = 10;
 
-    NN* nn = createNN(inDim, hiddenDim1, hiddenDim2, outDim);
+    NN* nn = createNN(inFeatures, 3, [64, 16, 10]);
     
-    size_t sizeIn = sizeof(float) * inDim;
+    size_t sizeIn = sizeof(float) * inFeatures;
 
     float* h_in = (float*)malloc(sizeIn);
-    initMatrixRandom(h_in, inDim);
+    initMatrixRandom(h_in, inFeatures);
 
     float* d_in;
-    err = cudaMalloc(&d_in, sizeIn);
-    if (err != cudaSuccess) {
-        printf("error `cudaMalloc`ing d_in. error: %s\n", cudaGetErrorString(err));
-    }
+    cudaError_t err1 = cudaMalloc(&d_in, sizeIn);
+    if (err1 != cudaSuccess)
+        printf("error `cudaMalloc`ing d_in. error: %s\n", cudaGetErrorString(err1));
 
-    err = cudaMemcpy(d_in, h_in, sizeIn, cudaMemcpyHostToDevice);
-    if (err != cudaSuccess) {
-        printf("error `cudaMemcpy`ing h_in to d_in. error: %s\n", cudaGetErrorString(err));
-    }
+    cudaError_t err2 = cudaMemcpy(d_in, h_in, sizeIn, cudaMemcpyHostToDevice);
+    if (err2 != cudaSuccess)
+        printf("error `cudaMemcpy`ing h_in to d_in. error: %s\n", cudaGetErrorString(err2));
 
-    size_t sizeOut = sizeof(float) * outDim;
+    size_t sizeOut = sizeof(float) * outFeatures;
 
     float* d_out;
-    err = cudaMalloc(&d_out, sizeOut);
-    if (err != cudaSuccess) {
-        printf("error `cudaMalloc`ing d_out. error: %s\n", cudaGetErrorString(err));
-    }
+    cudaError_t err3 = cudaMalloc(&d_out, sizeOut);
+    if (err3 != cudaSuccess)
+        printf("error `cudaMalloc`ing d_out. error: %s\n", cudaGetErrorString(err3));
 
     runNN(nn, d_in, d_out);
+    freeNN(nn);
 
     float* h_out = (float*)malloc(sizeOut);
-    err = cudaMemcpy(h_out, d_out, sizeOut, cudaMemcpyDeviceToHost);
-    if (err != cudaSuccess) {
-        printf("error `cudaMemcpy`ing d_out to h_out. error: %s\n", cudaGetErrorString(err));
-    }
+    cudaError_t err4 = cudaMemcpy(h_out, d_out, sizeOut, cudaMemcpyDeviceToHost);
+    if (err4 != cudaSuccess)
+        printf("error `cudaMemcpy`ing d_out to h_out. error: %s\n", cudaGetErrorString(err4));
 
     // ==========
 
     printf("First few elements of `out`:\n");
-    for (int i = 0; i < outDim; ++i)
+    for (int i = 0; i < outFeatures; ++i)
         printf("%f ", h_out[i]);
 
 }
